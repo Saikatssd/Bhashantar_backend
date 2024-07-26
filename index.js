7510834633044fc2523369fc62822e7e07b42a23
Saikat Sundar Das <92164254+Saikatssd@users.noreply.github.com>
2024-07-27 02:07:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL,
+  origin:  ["https://bhashantar-frontend.vercel.app", process.env.FRONTEND_URL],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
