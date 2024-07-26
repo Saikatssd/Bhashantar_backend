fd30e442481baaca4be5fc366b74153737efa359
Saikat Sundar Das <92164254+Saikatssd@users.noreply.github.com>
2024-07-26 17:24:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
